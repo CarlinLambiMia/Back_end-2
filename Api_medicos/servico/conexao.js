342e483d19c24ae350c18c590e1ecc744c24677b
CarlinLambiMia <cego.judo@gmail.com>
2025-02-06 11:15:54 -0400

    n

--- Api_medicos/servico/conexao.js
+++ Api_medicos/servico/conexao.js
@@ -2,8 +2,8 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'Nathan',
-    password: '123456',
+    user: 'Api_ipoclinica',
+    password: 'Bolacha90**',
     database: 'clinica'
 });
 
